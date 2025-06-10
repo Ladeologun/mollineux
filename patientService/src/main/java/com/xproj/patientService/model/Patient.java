aaae0a254f323bbc4e4245df23f82abaf08e14a2
Ladeologun <ologunomolade21@gmail.com>
2025-06-10 18:22:58 +0100

    feat:set up patient service and database connection

---
+++ patientService/src/main/java/com/xproj/patientService/model/Patient.java
@@ -0,0 +1,25 @@
+package com.xproj.patientService.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private LocalDate dateOfBirth;
+    @Column(nullable = false)
+    private LocalDate registrationDate;
+}
