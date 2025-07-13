8d669cca1951ac135b2e647f0f60d1089963d6fa
Ladeologun <ologunomolade21@gmail.com>
2025-07-13 15:24:44 +0100

    chore:added api gateway to the setup

---
+++ apiGateway/src/test/java/com/xproj/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xproj.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
